264099bb979862e48d2332c2aa82350f15efe4d7
ankitapatelanki <ankitapatel11068@gmail.com>
2026-05-27 14:08:00 +0530

    "Updated middleware and router files"

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -1,5 +1,49 @@
 const jwt = require('jsonwebtoken');
+const joi = require('joi');
 
+
+// register
+const registerValidation = async (req, res, next) => {
+    console.log("request body",req.body);
+    const schema = joi.object({
+        name: joi.string().required(),
+         email: joi.string().email().required(),
+        password: joi.string().min(6).required()
+    });
+    try{
+        const value =await schema.validateAsync(req.body);
+        console.log("validation successful", value )
+        next();
+    } catch (error) {
+         console.log(" Error Message:", error.details[0].message);
+        res.status(400).json({
+            message: error.details[0].message
+        });
+    }
+
+}
+// login schema
+const loginValidation = async (req, res, next) => {
+    console.log("request body ", req.body);
+    const schema = joi.object({
+        email: joi.string().email().required(),
+        password: joi.string().min(6).required()
+    });
+    try{
+        const value = await schema.validateAsync(req.body);
+        console.log("validation successful", value)
+        next();
+    }catch(error){
+        console.log("error mmessage: ", error.details[0].message);
+        res.status(400).json({
+            message: error.details[0].message
+        });
+    }
+}
+
+
+
+// get 
 const tokenVerification = (req, res, next) => {
 
     try {
@@ -40,4 +84,7 @@ const tokenVerification = (req, res, next) => {
 
 };
 
-module.exports = { tokenVerification };
+module.exports = {  registerValidation, loginValidation, tokenVerification };
+
+
+
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const { register ,login,getUserData} = require('../controller/controller');
-const { tokenVerification } = require('../middleware/middleware');
+const {registerValidation, loginValidation, tokenVerification } = require('../middleware/middleware');
 
 // Importing route handlers
 // Define routes
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', registerValidation, register);
+// router.post('/register', register);
+
+router.post('/login', loginValidation, login)
+// router.post('/login', login);
+
+
 router.get('/getuserdata/:id', tokenVerification, getUserData);
 
 module.exports = router;
